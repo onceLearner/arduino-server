a4119074255434d952e44b43f79d1047473e0dad
onceLearner <redaamir972@gmail.com>
2021-12-06 00:43:59 +0100

    new one ' '

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express")
+
+const app = express()
+
+const fs = require("fs")
+
+app.get("/", (req, res) => {
+    res.send("<h1>hello world </h1>")
+
+})
+
+app.get("/read", (req, res) => {
+
+    var content = fs.readFileSync("test.txt");
+    res.json({ content: content.toString() })
+
+})
+
+
+const Port = process.env.PORT || 5001
+
+
+const run = () => {
+    app.listen(Port, () => {
+        console.log("app is running under port ", Port)
+    })
+}
+
+run()
